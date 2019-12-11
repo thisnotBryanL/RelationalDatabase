44631c5d40c7a04bac8bc29a2f9a517099b2f0bb
micahdadson <Micah_Dadson1@baylor.edu>
2019-12-10 22:57:55 -0600

    I created the tables. They need to be tweaked slightly inorder to maintain integrity constraints

--- Marquise_Working_Flask/TableSchema.py
+++ Marquise_Working_Flask/TableSchema.py
@@ -12,7 +12,7 @@ def createTables(mycursor):
                      "ADV_PR_year CHAR(4),"
                      "PRIMARY KEY (BaylorID))")
 
-    mycursor.execute("CREATE TABLE IF NOT EXISTS Internship(company VARCHAR (50),"
+    mycursor.execute("CREATE TABLE IF NOT EXISTS Internship(supervisorEmail VARCHAR (30),"
                      "startMonth VARCHAR (15),"
                      "startYear CHAR (4),"
                      "endMonth VARCHAR (15),"
@@ -21,57 +21,106 @@ def createTables(mycursor):
                      "phoneNumber CHAR(11),"
                      "totalHours INT,"
                      "BaylorID CHAR(9),"
-                     "supervisorName VARCHAR(50),"
-                     "PRIMARY KEY (BaylorID, company, supervisorName),"
-                     #"FOREIGN KEY (supervisorName) REFERENCES Supervisor(supervisorName)," 
-                     #"FOREIGN KEY (company) REFERENCES Supervisor(company,)"
+                     #"supervisorName VARCHAR(50)," #supervisorName and company replaced with SupervisorEmail
+                     "PRIMARY KEY (BaylorID, supervisorEmail),"
                      #Supervisor has not yet been created so will have to alter Internship table after 
-                     #Supervisor table has been created to add forteign key constraint
+                     #to add supervisorEmail foreign constraint
                      "FOREIGN KEY (BaylorID) REFERENCES StudentInfo(BaylorID))")
 
     mycursor.execute("CREATE TABLE IF NOT EXISTS Supervisor(company VARCHAR (50),"
                      "supervisorName VARCHAR (50),"
                      "title VARCHAR (20),"
                      "email VARCHAR (30),"
-                     "UNIQUE (supervisorName),"
-                     #made supervisorName unique so other tables could reference it
-                     #I don't think it should be a problem, though it may be possible
-                     #that mult students have the same supervisor It's unlikely
+                     "studentID CHAR (9),"
+                     "FOREIGN KEY (studentID) REFERENCES StudentInfo (BaylorID),"
                      "PRIMARY KEY (email))")
-                     #"FOREIGN KEY (company) REFERENCES Internship(company))")
 
-    mycursor.execute("CREATE TABLE IF NOT EXISTS Questions(label VARCHAR (50),"
+    mycursor.execute("CREATE TABLE IF NOT EXISTS SupervisorQuestions(label VARCHAR (50),"
                      "question VARCHAR (255),"
                      "comments VARCHAR (255),"
                      "startYear CHAR (4),"
-                     "PRIMARY KEY (question))")
-                     #made question, label composite instead of just label as pk so label could be referenced
-                    #from Review
+                     "isOrdered BIT," #if question has order associated with it, answers have values
+                     "PRIMARY KEY (label, startYear, isOrdered))")
 
-
-    mycursor.execute("CREATE TABLE IF NOT EXISTS Review(reviewType VARCHAR (50),"
-                     "supervisorName VARCHAR (50),"
-                     #added question so questionLabel could reference label from Questions
+    mycursor.execute("CREATE TABLE IF NOT EXISTS studentQuestions(label VARCHAR (50),"
                      "question VARCHAR (255),"
-                     "baylorID CHAR (9),"
-                     "questionLabel VARCHAR (50),"
-                     "PRIMARY KEY (reviewType, supervisorName, baylorID, questionLabel),"
-                     "FOREIGN KEY (baylorID) REFERENCES StudentInfo(BaylorID),"
-                     #"FOREIGN KEY (questionLabel) REFERENCES Questions(label),"
-                     "FOREIGN KEY (supervisorName) REFERENCES Supervisor(supervisorName))")
+                     "comments VARCHAR (255),"
+                     "startYear CHAR (4),"
+                     "isOrdered BIT," #if question has order associated with it, answers have values
+                     "PRIMARY KEY (label, startYear, isOrdered))")
 
 
-    mycursor.execute("CREATE TABLE IF NOT EXISTS Answers(baylorID CHAR (9),"
+    mycursor.execute("CREATE TABLE IF NOT EXISTS QualtricsReview(baylorID CHAR (9),"
+                     "reviewTime VARCHAR(20)," #either midterm or endterm
                      "answer VARCHAR (255),"
-                     "label VARCHAR (255),"
-                     "question VARCHAR (255),"
-                     "PRIMARY KEY (baylorID, answer, question),"
+                     "label VARCHAR (255)," #each answer is associated with a question (label, year)
+                     "syear CHAR (4),"
+                     "comment VARCHAR (255),"
+                     "isOrdered BIT,"
+                     "PRIMARY KEY (baylorID, reviewTime, label, syear)," #can identify answer with these 4 attributes
                      "FOREIGN KEY (baylorID) REFERENCES StudentInfo(BaylorID),"
-                     "FOREIGN KEY (question) REFERENCES Questions(question))")
+                     "FOREIGN KEY (label, syear, isOrdered) REFERENCES SupervisorQuestions(label, startYear, isOrdered))")
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS midTermSiteReview(baylorID CHAR (9),"
+                     "answer VARCHAR (255),"
+                     "label VARCHAR (255)," #each answer is associated with a question (label, year)
+                     "syear CHAR (4),"
+                     "comment VARCHAR (255),"
+                     "isOrdered BIT,"
+                     "PRIMARY KEY (baylorID, label, syear)," #can identify answer with these 3 attributes
+                     "FOREIGN KEY (baylorID) REFERENCES StudentInfo(BaylorID),"
+                     "FOREIGN KEY (label, syear) REFERENCES SupervisorQuestions(label, startYear))")
+
+    mycursor.execute("CREATE TABLE IF NOT EXISTS StudentReview(baylorID CHAR (9),"
+                     "answer VARCHAR (255),"
+                     "label VARCHAR (255)," #each answer is associated with a question (label, year)
+                     "syear CHAR (4),"
+                     "comment VARCHAR (255),"
+                     "isOrdered BIT,"
+                     "PRIMARY KEY (baylorID, label, syear)," #can identify answer with these 3 attributes
+                     "FOREIGN KEY (baylorID) REFERENCES StudentInfo(BaylorID),"
+                     "FOREIGN KEY (label, syear) REFERENCES StudentQuestions(label, startYear))")
 
 
-    #mycursor.execute("ALTER TABLE Internship ADD FOREIGN KEY (company, supervisorName) REFERENCES Supervisor(company, supervisorName)")
+    mycursor.execute("ALTER TABLE Internship ADD FOREIGN KEY (supervisorEmail) REFERENCES Supervisor(email)")
 
+
+#get the basic student info
+#if they enter id
+basicInfoBaylorID = """SELECT BaylorID, lastName, firstName, emailAddress, ADV_PR_semester, class, major_minor, ADV_PR_grade, ADV_PR_year
+FROM StudentInfo
+WHERE BaylorID = %s"""
+#if they enter name
+basicInfoName = """SELECT BaylorID, lastName, firstName, emailAddress, ADV_PR_semester, class, major_minor, ADV_PR_grade, ADV_PR_year
+FROM StudentInfo
+WHERE firstName = %s AND lastName = %s"""
+
+#ask user for which type of review they would like
+#if review type is Qualtrics
+#ask user if it is midterm or endterm
+#ask if they want to enter the year as well, if no:
+displayQReviewIDNoYear = "SELECT answer, comment FROM QualtricsReview WHERE reviewTime = %s AND BaylorID = %s"
+displayQReviewnameNoYear = """SELECT answer, comment 
+FROM QualtricsReview, StudentInfo 
+WHERE reviewTime = %s AND firstName = %s AND lastName = %s AND QualtricsReview.baylorID = StudentInfo.baylorID"""
+#if yes:
+displayQReviewIDYear = "SELECT answer, comment FROM QualtricsReview WHERE reviewTime = %s AND BaylorID = %s AND syear = %s"
+displayQReviewnameYear = """SELECT answer, comment 
+FROM QualtricsReview, StudentInfo 
+WHERE reviewTime = %s AND firstName = %s AND lastName = %s AND QualtricsReview.baylorID = StudentInfo.baylorID AND year = %s"""
+
+#if review type is midTermSiteReview
+#ask if they want to enter the year as well, if no:
+displayMReviewIDNoYear = "SELECT answer, comment FROM midTermSiteReview WHERE BaylorID = %s"
+displayMReviewnameNoYear = """SELECT answer, comment 
+FROM midTermSiteReview, StudentInfo 
+WHERE firstName = %s AND lastName = %s AND midTermSiteReview.baylorID = StudentInfo.baylorID"""
+#if yes:
+displayMReviewIDYear = "SELECT answer, comment FROM midTermSiteReview WHERE BaylorID = %s AND syear = %s"
+displayMReviewnameYear = """SELECT answer, comment 
+FROM midTermSiteReview, StudentInfo 
+WHERE reviewTime = %s AND firstName = %s AND lastName = %s AND midTermSiteReview.baylorID = StudentInfo.baylorID AND year = %s"""
+#
 
 
 def insertIntoStudentInfo(idEntry, lastnameEntry, firstnameEntry, emailEntry, semesterEntry, classyr, major_minor, grade, year, mycursor, mydb, middleFrame, topFrame):
