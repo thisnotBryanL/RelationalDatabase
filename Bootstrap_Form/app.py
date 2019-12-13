3a18b8c9271828700ebd0f178d2f515bc39104f8
micahdadson <Micah_Dadson1@baylor.edu>
2019-12-12 23:18:24 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Bootstrap_Form/app.py

--- Bootstrap_Form/app.py
+++ Bootstrap_Form/app.py
@@ -4,16 +4,14 @@ from flask_table import Table, Col,LinkCol
 from wtforms import StringField, SelectField, SubmitField
 from wtforms.validators import InputRequired, Email, Length, DataRequired
 from flask_bootstrap import Bootstrap
-
-
-from Bootstrap_Form.TableSchema import createTables
+from TableSchema import *
 import mysql.connector
 
 #connect to database
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "hoangdieu72",
+    password = "password123",
     database = "GP"
 )
 
@@ -30,19 +28,18 @@ app.config['SECRET_KEY'] = 'DontTellAnyone'
 
 # Create Classes for forms and web pages
 
-year_list = [(0, '---')]
-for i in range(20):
-    year_list.append((i + 1, 2000 + i))
-
 class StudentInfoForm(FlaskForm):
-    student_id = StringField('Student ID', validators=[InputRequired(), Length(9)])
+    student_id = StringField('student ID', validators=[InputRequired(), Length(9)])
     first_name = StringField('First Name', validators=[InputRequired()])
     last_name = StringField('Last Name', validators=[InputRequired()])
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email address')])
+    email = StringField('email', validators=[InputRequired(), Email(message='Invalid email address')])
     Class = StringField('Class', validators=[InputRequired()])
 
 class StudentInfoForm2(FlaskForm):
     grade_list = [(0, '---'), (1, 'A'), (2, 'B'), (3, 'C'), (4, 'D'), (5, 'F')]
+    year_list = [(0, '---')]
+    for i in range(20):
+        year_list.append((i + 1, 2000 + i))
     major_minor = SelectField('Major/Minor', [DataRequired()], choices=[(0, "---"), (1, 'major'), (2, 'minor')])
     ADV_PR_Semester = SelectField('ADV PR Semester', choices=[(0, '---'), (1, 'Fall'), (2, 'Spring')])
     ADV_PR_Year = SelectField('Year', choices=year_list)
@@ -63,19 +60,19 @@ class SupervisorInfoForm(FlaskForm):
     title = StringField('Title', validators=[InputRequired()])
     email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email address')])
 
-class InternshipInfoForm(FlaskForm):
-    month_list = [(0,'---'), (1,'JAN'), (2,'FEB'), (3,'MAR'), (4,'APR'), (5,'MAY'),
-                  (6,'JUN'), (7,'JUL'), (8,'AUG'), (9,'SEP'), (10,'OCT'),
-                  (11, 'NOV'), (12, 'DEC')]
-
-    email = StringField('Email', validators=[InputRequired(), Email(message='Invalid email address')])
-    startMonth = SelectField('Start Month', choices=month_list)
-    startYear = SelectField('Start Year', choices=year_list)
-    endMonth = SelectField('End Month', choices=month_list)
-    endYear = SelectField('End Year', choices=year_list)
-
 
 class Results(Table):
+    id = Col('Baylor ID ')
+    fname = Col('First Name ')
+    lname = Col('Last Name ')
+    email = Col('Email ')
+    semester = Col('Semester' )
+    yr = Col('Year ')
+    major_minor = Col('Major ')
+    grade = Col('Grade ')
+    classYear = Col('Class')
+
+class ResultsSup(Table):
     id = Col('Baylor ID ')
     fname = Col('First Name ')
     lname = Col('Last Name ')
@@ -128,7 +125,7 @@ class PortfolioReviewItem(object):
         self.reviewerName = reviewerName
 
 class Item(object):
-    def __init__(self, id, fname, lname, email,semester,yr,major_minor,grade,classYear):
+    def __init__(self, id, fname, lname, email, semester, yr, major_minor, grade, classYear):
         self.id = id
         self.fname = fname
         self.lname = lname
@@ -138,6 +135,17 @@ class Item(object):
         self.major_minor = major_minor
         self.grade = grade
         self.classYear = classYear
+
+    def setValues (self, list):
+        self.id = list[0]
+        self.fname = list[2]
+        self.lname = list[1]
+        self.email = list[3]
+        self.semester = list[4]
+        self.yr = list[8]
+        self.major_minor = list[6]
+        self.grade = list[7]
+        self.classn = list[5]
 
 class YearSearchForm(FlaskForm):
     year = StringField('Enter Year:')
@@ -152,8 +160,6 @@ def index():
             return redirect(url_for('studentInfo'))
         elif request.form['option'] == 'Enter Supervisor Information':
             return redirect(url_for('supervisorInfo'))
-        elif request.form['option'] == 'Enter Internship Information':
-            return redirect(url_for('internshipInfo'))
     return render_template('index.html')
 
 
@@ -174,13 +180,6 @@ def supervisorInfo():
         return 'Successfully submitted supervisor information!'
     return render_template('supervisor.html', form=form)
 
-@app.route('/input_internship_info', methods=['GET', 'POST'])
-def internshipInfo():
-    form = InternshipInfoForm()
-    if form.validate_on_submit():
-        return 'Successfully submitted internship information!'
-    return render_template('internship.html', form=form)
-
 
 ######################## SUDENT QUERY DATA ########################
 
@@ -191,50 +190,58 @@ def studentQueryHomePage():
         return search_results(search)
     return render_template('studentQueryHome.html', form=search)
 
-
 @app.route('/studentQuery/results')
 def search_results(search):
     results = []
+    choice = " "
+    stringf = " "
+
 
     if search.select.data == "Baylor ID":
         print("BUID")
+        choice = "idTrue"
         #Query the BU ID
         search_string = search.data['search']
+        results.append(search_string)
+        stringf = search_string
 
     else:
         print("BUNAME")
         firstNameSearch = search.data['firstName']
         lastNameSearch = search.data['lastName']
+        choice = "nameTrue"
+        results.append(firstNameSearch)
+        results.append(lastNameSearch)
+        stringf = firstNameSearch + " " + lastNameSearch
 
-    if search.data['search'] == 'Bryan Lee':
-        items = [Item('000000000', 'Bryan', 'Lee', 'Bryan_Lee@baylor.edu', 'Fall', '2019', 'PR', 'A', 'SR'),
-                 Item('000000000', 'Bryan', 'Lee', 'Bryan_Lee@baylor.edu', 'Fall', '2019', 'PR', 'A', 'SR'),
-                 Item('111111111', 'Bryan', 'Lee', 'Bryan_Lee@baylor.edu', 'Fall', '2019', 'PR', 'A', 'SR')]
+    queryResults = []
+    queryResults = basicInfo(mycursor, mydb, choice, results)
+    print("query results", queryResults)
+
+    if len(queryResults) == 0:
+        flash('No results found!')
+        return redirect('/studentQuery')
+
+
+    elif search.data['search'] == stringf:
+        items = []
+        for row in queryResults:
+            instance = Item('', '', '', '', '', '', '', '', '')
+            instance.setValues(row)
+            items.append(instance)
         table = Results(items)
         table.border = True
         return render_template('results.html', table=table)
-    #qry = db_session.query(Album)
-    #results = qry.all()
 
-    if len(results) == 0:
-        flash('No results found!')
-        return redirect('/studentQuery')
-    else:
-        # display results
-        #table.border = True
-        return render_template('results.html', table = table)
-
-@app.route('/studentQuery/results/reviews/year', methods=['GET','POST'])
-def SearchYearHomePage():
-    yearSearch = YearSearchForm()
-    if request.method == 'POST' and yearSearch.validate_on_submit():
-        return SearchYear(yearSearch)
-    return render_template('yearForm.html', form=yearSearch)
-
-def SearchYear(search):
-    yearEntered = search.data['year']
-
-
+    elif search.data['firstname'] == firstNameSearch and search.data['lastname'] == lastNameSearch:
+        items = []
+        for row in queryResults:
+            instance = Item('', '', '', '', '', '', '', '', '',)
+            instance.setValues(row)
+            items.append(instance)
+        table = Results(items)
+        table.border = True
+        return render_template('results.html', table=table)
 
 
 
@@ -284,7 +291,7 @@ def portfolioReviewLink(id):
             results = [PortfolioReviewItem('QUESTION 1', 'ANSWER1',
                                             'This is a commnt that is supposed to be kind of l'
                                             'ong to see how this would fit in to the table '
-                                            'lol hahahahahahha hehehehehe hohohohohohohho','Name of reviewer')]  # THE query information
+                                            'lol hahahahahahha hehehehehe hohohohohohohho')]  # THE query information
 
             portfolioReviewTable = PortfolioReviewTable(results)
             portfolioReviewTable.border = True
