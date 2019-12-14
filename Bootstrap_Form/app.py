2e3e35e3e6773c9620b17d7fb4739b96a9a8d10f
Bryan Lee <bryan_lee@baylor.edu>
2019-12-14 02:46:07 -0600

    Updated student search page

--- Bootstrap_Form/app.py
+++ Bootstrap_Form/app.py
@@ -14,7 +14,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
     host = "localhost",
     user = "root",
-    password = "password123",
+    password = "hoangdieu72",
     database = "GP"
 )
 
